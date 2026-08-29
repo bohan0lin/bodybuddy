229e02a9c33406b98bb4f87d15af381d552f53e5
bohan0lin <bohan0lin@gmail.com>
2026-08-28 23:48:37 -0400

    chore: 移除诊断探针 /api/ping

--- api/ping.ts
+++
@@ -1,5 +0,0 @@
-// 零依赖诊断探针
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export default function handler(_req: any, res: any) {
-  res.status(200).json({ ok: true, node: process.version })
-}
